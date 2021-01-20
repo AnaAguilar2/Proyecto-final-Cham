1a6c27819865ef8a4701055c72c1de99a2f36f49
AnaAguilar2 <42815813+AnaAguilar2@users.noreply.github.com>
2021-01-19 18:48:52 -0600

    Add files via upload

---
+++ Indexstart.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cham</title>
+	<link rel="stylesheet" type="text/css" href="css/start_estilos1.css">
+</head>
+<body>
+	<div class="caja">
+	<header >
+		<img class="logo" src="img/logo1.png">
+		<nav>
+			<ul class="naveg_liks">
+				<!--<li class="navegar"><a href="#" class="navegar">Servicio</a></li>
+				<li class="navegar"><a href="index.html" class="navegar">Nosotros</a></li>-->
+				<li class="navegar"><a href="index.html" class="navegar">Nosotros</a></li>
+			</ul>
+		</nav>
+		<a class="log" href="login_php/singup.php"><button class="navegar">Login</button></a>
+	</header>
+	</div>
+	<div class="start">
+	<a href="login_php/login.php" class="anima">
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		Iniciar web
+	</a>
+	</div>
+</body>
+</html>
